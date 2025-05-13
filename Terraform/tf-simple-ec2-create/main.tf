fe57fd6bb6ead665c72af1cf08418e198955b7fe
Saanil <saaniltaneja@gmail.com>
2025-05-13 12:26:06 +0530

    Added Kubernetes

--- Terraform/tf-simple-ec2-create/main.tf
+++ Terraform/tf-simple-ec2-create/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-	source        = "/home/saaniltaneja/Projects/Terraform/tf-simple-ec2-create/modules/ec2_instance"
+	source        = "/home/saaniltaneja/Projects/Terraform/tf-simple-ec2-create/modules/ec2_instance/"
 	instance_type = "t2.micro"
 	ami           = "ami-0e35ddab05955cf57"
 	subnet_id     = "subnet-04cb69f4d9762dbe0"
